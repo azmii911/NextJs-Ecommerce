49f075c1646687b499e5cd3e56c9f3f872c46223
Raheel Azmi <raheelazmi58@gmail.com>
2022-07-20 13:59:11 +0500

    pushedagain

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 const dev = process.env.NODE_ENV === "production";
 
 export const API_BASE_URL = dev
-  ? "https://next-js-ecommerce-ivory.vercel.app/api"
-  : "https://next-js-ecommerce-ivory.vercel.app/";
+  ? "https://next-js-ecommercee-ivory.vercel.app/api"
+  : "https://next-js-ecommercee-ivory.vercel.app/";
 export const BASE_URL = dev
   ? "http://localhost:3000"
-  : "https://next-js-ecommerce-ivory.vercel.app/";
+  : "https://next-js-ecommercee-ivory.vercel.app/";
