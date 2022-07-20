e4f8e0b97a8f80f34d9f5d836037af0dc32533ab
Raheel Azmi <raheelazmi58@gmail.com>
2022-07-20 14:39:13 +0500

    domain chngee

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const dev = process.env.NODE_ENV === "production";
 
 export const API_BASE_URL = dev
   ? "http://localhost:3000/api"
-  : "https://next-js-ecommerce-one.vercel.app/api";
+  : "https://next-js-ecommerce-ebon.vercel.app/api";
 export const BASE_URL = dev
   ? "http://localhost:3000"
-  : "https://next-js-ecommerce-one.vercel.app";
+  : "https://next-js-ecommerce-ebon.vercel.app/";
