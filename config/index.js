ac221f0b6e27511213fc37a94d454d7219875177
Raheel Azmi <raheelazmi58@gmail.com>
2022-07-20 13:53:50 +0500

    domain name change

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV === "production";
 
 export const API_BASE_URL = dev
-  ? "http://localhost:3000/api"
-  : "https://your_deployment.server.com";
+  ? "https://next-js-ecommerce-ivory.vercel.app/api"
+  : "https://next-js-ecommerce-ivory.vercel.app/";
 export const BASE_URL = dev
   ? "http://localhost:3000"
-  : "https://your_deployment.server.com";
+  : "https://next-js-ecommerce-ivory.vercel.app/";
