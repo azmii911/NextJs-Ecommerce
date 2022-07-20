8d7e8a7b2f9e3896872232c7fedf4f249e94b081
Raheel Azmi <raheelazmi58@gmail.com>
2022-07-20 15:17:35 +0500

    asdsadsadasd

--- config/index.js
+++ config/index.js
@@ -1,8 +1,4 @@
 const dev = process.env.NODE_ENV === "production";
 
-export const API_BASE_URL = dev
-  ? "http://localhost:3000/api"
-  : "https://next-js-ecommerce-ebon.vercel.app/api";
-export const BASE_URL = dev
-  ? "http://localhost:3000"
-  : "https://next-js-ecommerce-ebon.vercel.app";
+export const API_BASE_URL = "https://next-js-ecommerce-ebon.vercel.app/api";
+export const BASE_URL = "https://next-js-ecommerce-ebon.vercel.app";
